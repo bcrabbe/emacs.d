d39e93af6a361347684b6b36d9520e73acd75553
bcrabbe <stilled_flyer@hotmail.com>
2018-05-14 17:47:59 +0100

    add packages

---
+++ elpa-25.1/dhall-mode-20171204.1327/dhall-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dhall-mode" "20171204.1327" "a major mode for dhall configuration language" '((emacs "24.4")) :commit "bc6aec777594beeac6ba4c6dbfb1c889341589c9" :url "https://github.com/psibi/dhall-mode" :keywords '("languages"))
---
+++ elpa-25.1/grab-mac-link-20180116.251/grab-mac-link-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "grab-mac-link" "20180116.251" "Grab link from Mac Apps and insert it into Emacs" '((emacs "24")) :commit "19369badf8b0621eb03ea9e3adeecb22b9710c23" :url "https://github.com/xuchunyang/grab-mac-link.el" :keywords '("mac" "hyperlink"))
