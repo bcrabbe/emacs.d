0f58bd50a30a323ce696f184da74c1bf141686ec
Ben Crabbe <bencrabbe@hotmail.co.uk>
2018-07-31 09:32:55 +0100

    add key-chord

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,8 @@
 (load-theme 'zenburn t)
 (load "./hl-tags-mode.el")
 (setq Preferred-javascript-mode 'rjsx-mode)
+(require 'key-chord)
+(key-chord-mode +1)
 
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
@@ -10,5 +12,19 @@
 (add-hook 'rjsx-mode-hook 'hl-tags-mode)
 
 (global-set-key (kbd "M-g r") 'find-grep-dired)
+
+
+
+(key-chord-define-global "qq" 'split-window-horizontally)
+(global-set-key (kbd "C-x 4") 'split-window-horizontally)
+
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(key-chord-define-global "JJ" 'switch-to-previous-buffer)
+
 
 (provide 'init-local)
