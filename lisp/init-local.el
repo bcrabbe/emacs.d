6d0d4b2b686a656cca1321408bc8668f7f985bd7
Ben Crabbe <bencrabbe@hotmail.co.uk>
2018-08-18 07:25:32 +0100

    updated for emacs 26.1

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,15 @@
-(load-theme 'zenburn t)
 (load "./hl-tags-mode.el")
+(require-package 'zenburn-theme)
+(require-package 'emmet-mode)
+(require-package 'rainbow-mode)
+(require-package 'key-chord)
+(require-package 'rjsx-mode)
+
+
+(load "../themes/zenburn-theme.el")
+
+;; (setq-default custom-enabled-themes '(zenburn-theme))
+
 (setq Preferred-javascript-mode 'rjsx-mode)
 (require 'key-chord)
 (key-chord-mode +1)
@@ -10,8 +20,9 @@
 (setq emmet-expand-jsx-className? t) ;; default nil
 (add-hook 'rjsx-mode-hook 'emmet-mode)
 (add-hook 'rjsx-mode-hook 'hl-tags-mode)
+(add-hook 'rjsx-mode-hook 'rainbow-mode)
 
-(global-set-key (kbd "M-g r") 'find-grep-dired)
+(global-set-key (kbd "M-g r") 'rgrep)
 
 (defun split-window-and-balance ()
   "Split and balance: \"split-window-horizontally\" and then \"balance-windows\"."
@@ -19,10 +30,16 @@
   (split-window-horizontally)
   (balance-windows))
 
-(key-chord-define-global "qq" 'split-window-and-balance)
+(key-chord-define-global "§§" 'split-window-and-balance)
 (global-set-key (kbd "C-x 4") 'split-window-horizontally)
-(key-chord-define-global "pp" 'balance-windows)
 
+(defun error-logger-info-report ()
+  "Insert error_logger call."
+  (interactive)
+  (insert "error_logger:info_report([{module,?MODULE},
+                                     {line,?LINE},
+                                     {}])"))
+(key-chord-define-global "LL" 'error-logger-info-report)
 
 (defun switch-to-previous-buffer ()
   "Switch to previously open buffer.
@@ -32,5 +49,7 @@ Repeated invocations toggle between the two most recently open buffers."
 
 (key-chord-define-global "JJ" 'switch-to-previous-buffer)
 
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
 
 (provide 'init-local)
